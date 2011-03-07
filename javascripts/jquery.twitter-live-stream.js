a6007b8dd89ced1265e99bebcb8a493772b5b2b7
Ken Mazaika <kmazaika@gmail.com>
2011-03-06 19:27:44 -0500

    should be a working twitter stream example app.

--- javascripts/jquery.twitter-live-stream.js
+++ javascripts/jquery.twitter-live-stream.js
@@ -1,18 +1,14 @@
 TwitterAPI = {
-	Statuses: {
-		
-		search:function(query, count, callback){
-			jQuery.getJSON("http://search.twitter.com/search.json?&q=" + query + "&count="+count+"&cb="+Math.random()+"&callback=?", callback);
+	Statuses: {		
+		search:function(query, callback){
+			jQuery.getJSON("http://search.twitter.com/search.json?&q=" + query + "&cb="+Math.random()+"&callback=?", callback);
 		}
-		
 	}
 };
 
-
-
 window.ify=function(){var entities={'"':'"','&':'&','<':'<','>':'>'};return{"link":function(t){return t.replace(/[a-z]+:\/\/[a-z0-9-_]+\.[a-z0-9-_:~%&\?\/.=]+[^:\.,\)\s*$]/ig,function(m){return'<a href="'+m+'">'+((m.length>25)?m.substr(0,24)+'...':m)+'</a>';});},"at":function(t){return t.replace(/(^|[^\w]+)\@([a-zA-Z0-9_]{1,15})/g,function(m,m1,m2){return m1+'@<a href="http://twitter.com/'+m2+'">'+m2+'</a>';});},"hash":function(t){return t.replace(/(^|[^\w'"]+)\#([a-zA-Z0-9_]+)/g,function(m,m1,m2){return m1+'#<a href="http://search.twitter.com/search?q=%23'+m2+'">'+m2+'</a>';});},"clean":function(tweet){return this.hash(this.at(this.link(tweet)));}};}();
-window.tweet_stream = function(query) {
-	TwitterAPI.Statuses.search(query,5,function(json){
+window.tweet_stream = function(query, broken_avatar, timeout) {
+	TwitterAPI.Statuses.search(query,function(json){
 		var content = $('#twitter-stream').html();
 	
 		var ids = new Array()
@@ -20,7 +16,10 @@ window.tweet_stream = function(query) {
 			var tweet = ify.clean(this['text']);
 			var d = new Date(this['created_at']);
 			var date_show = d.getDate()+'/'+d.getMonth()+'/'+d.getFullYear()+' '+d.getHours()+':'+d.getMinutes();
-			content = '<li id="'+ this['id'] + '">' + tweet + ' (<date>'+date_show+'</date>)</li>' + content;
+			content = '<li id="'+ this['id'] + '">' + 
+				'<img src="' + this['profile_image_url']+ '" onerror="this.src=\''+ broken_avatar +'\';"/><div class= "full-tweet">' + 
+			tweet + 
+			'<span class="info">'+ this['from_user']  + ' <date>'+date_show+'</date></span></div></li>' + content;
 			if(! $('#' + this['id']).length)
 				ids.push(this['id']);
 		});
@@ -28,9 +27,9 @@ window.tweet_stream = function(query) {
 		
 		$.each(ids, function(id) {
 			
-			$("#" + ids[id]).show('slow')
+			$("#" + ids[id]).show('fast')
 		})
 	});
-	setTimeout(function() { tweet_stream(query); }, 10000);
+	setTimeout(function() { tweet_stream(query); }, timeout*1000);
 	
 }
